bc5c1416e2fe771d8f5b98df133e6a943a703032
pgm-mehmbice <mehmbice@student.arteveldehs.be>
2021-06-10 12:08:48 +0200

    finish

---
+++ pgm_1_opdracht_2_Bicer_Mehmet/futureisland_app/app/js/data.js
@@ -0,0 +1,314 @@
+const listbar = [
+    {
+        name: 'home',
+        link: 'https://www.rockwerchter.be/en/'
+    },
+    {
+        name: 'line up',
+        link: 'https://www.rockwerchter.be/en/line-up'
+    },
+    {
+        name: 'tickets',
+        link: 'https://www.rockwerchter.be/en/tickets'
+    },
+    {
+        name: 'praktisch',
+        link: 'https://www.rockwerchter.be/en/practical'
+    },
+    {
+        name: 'camping',
+        link: 'https://www.rockwerchter.be/en/camping'
+    },
+    {
+        name: 'history',
+        link: 'https://www.rockwerchter.be/en/history'
+    },
+    {
+        name: 'rwtv',
+        link: 'https://www.proximus.be/pickx/nl/muziek/festivals/rockwerchter/vod'
+    },
+    {
+        name: 'nmv',
+        link: 'http://northwestwalls.be/'
+    },
+    {
+        name: 'buy tickets',  
+        link: 'https://www.ticketmaster.be/feature/rockwerchter/?language=en-us&utm_source=rockwerchter.be&utm_medium=website&utm_campaign=rw21&_ga=2.201035630.1129009412.1604435241-1272250283.1603792803'
+    },
+   
+];
+
+const logo= [
+    '1-4 july',
+    'festivalpark',
+    'werchter',
+    'belguim',
+];
+
+const artists = [
+  {
+    id: '892131aa-1d08-4750-92c0-356334cae44d',
+    artist: {
+        name: 'Pearl Jam',
+        synopsis: 'October 22, 2015 officially marked 25 years of Pearl Jam performing live. Ten studio albums, hundreds of unique live performances and official live concert bootleg releases later, the band continues to be critically acclaimed and commercially successful -- with over 85 million albums sold worldwide and a 2017 induction into the Rock and Roll Hall of Fame.',
+        social: {
+            website: 'https://pearljam.com/',
+            facebook: 'https://www.facebook.com/pearljam',
+            twitter: 'https://www.twitter.com/pearljam',
+            instagram: 'https://www.instagram.com/pearljam'
+            
+          },
+      
+      picture: {
+          small: 'https://assets.rockwerchter.be/files/cache/medium/files/14686-pearl-jam-032020-6c9a9964-flat-lowres-5f1a952633f63.jpg',
+          large: 'https://assets.rockwerchter.be/files/cache/medium/files/14686-pearl-jam-032020-6c9a9964-flat-lowres-5f1a952633f63.jpg'
+      },
+      media: [
+          {
+            type: 'Youtube',
+            sourceld: 'https://www.youtube.com/embed/qM0zINtulhM'
+          },
+      ],
+
+    
+    },
+      place: {
+          name: 'Main stage'
+      },
+      from: 1625090400000,
+      to: 1625090400000,
+      createdAt: 1603490400000 ,
+      modifietAt: 1604790000000,
+      isHeadliner: false,
+  },
+  {
+    id: '7a0cc1d9-1205-4aa4-ac10-def2ccb20259',
+    artist: {
+        name: 'alt-j',
+        synopsis: 'Hold down Alt + J on an Apple keyboard and you get Δ, the Greek letter ‘delta’. Mathematicians and physicists use this symbol to denote change. One might say that alt-J do something similar with pop history. The British trio picks the best moments of the last sixty years and subverts them within their own universe. They are just as committed and inventive as Sigur Rós or The xx. Their fairytale world is clearly at home at Werchter. As Humo wrote of their first appearance in 2013: ‘How much excruciating beauty can one person take, and how much sugary emotion? Alt-J changed your life in The Barn.’ And as Moustique witnessed in 2015: ‘Alt-J joins the big league.’ There has been a promising silence around the group since their last tour in 2017 …',
+        social: {
+            website: 'https://www.altjband.com/',
+            facebook: 'https://www.facebook.com/altJ.band',
+            twitter: 'https://www.twitter.com/alt_J',
+            instagram: 'https://www.instagram.com/unrealalt'
+          },
+      
+      picture: {
+          small: 'https://assets.rockwerchter.be/files/cache/medium/files/alt-j-5f030ee72f4ba.jpg',
+          large: 'https://assets.rockwerchter.be/files/cache/medium/files/alt-j-5f030ee72f4ba.jpg'
+      },
+      media: [
+          {
+            type: 'youtube',
+            sourceld: 'https://www.youtube.com/embed/GOJUNJ1o394'
+          },
+      ],
+      
+    },
+      place: {
+          name: 'Klub C'
+      },
+      from: 1625176800000,
+      to: 1625176800000,
+      createdAt: 1603490400000,
+      modifietAt: 1604790000000,
+      isHeadliner: false,
+  },
+  {
+    id: '83e41df8-1f6e-40ea-bae3-72683c33eaaf',
+    artist: {
+        name: 'Black Pumas',
+        synopsis: 'Imagine soul singer Sam Cooke and proto-rocker Neil Young in the Wu-Tang Clan hip hop collective. That should give you a rough idea of Black Pumas, a project run by an extraordinary duo from Austin, Texas. Adrian Quesada is in his forties and was once the guitarist for the nine-piece band Grupo Fantasma, winners of the Grammy for Best Latin Album in 2010. Quesada had a few instrumental tracks in his studio that were crying out for a soulful voice. Some friends recommended Eric Burton, a busker from downtown Austin, and the rest is history. The Black Pumas’ debut is just about flawless. Its psychedelic soul earned the two musicians the title of ‘breakout band of 2019’ in the US. Great stuff for anyone who has enjoyed Alabama Shakes, Durand Jones & The Indications or Curtis Harding at Werchter in recent years.',
+        social: {
+            website: 'https://www.theblackpumas.com/',
+            facebook: 'https://www.facebook.com/theblackpumas',
+            twitter: 'https://www.twitter.com/blackpumasmusic',
+            instagram: ''
+          },
+      
+      picture: {
+          small: 'https://assets.rockwerchter.be/files/cache/medium/files/black-pumas-5f030ee9dc31d.jpg',
+          large: 'https://assets.rockwerchter.be/files/cache/medium/files/black-pumas-5f030ee9dc31d.jpg'
+      },
+      media: [
+          {
+            type: 'Youtube',
+            sourceld: 'https://www.youtube.com/embed/2EntxPIULUI'
+          },
+      ],
+    },
+      place: {
+          name: 'Klub C'
+      },
+      from: 1625090400000,
+      to: 1625090400000,
+      createdAt: 1603490400000,
+      modifietAt: 1604790000000,
+      isHeadliner: false,
+  },
+  {
+    id: '7d7e8aa1-0366-4fe9-8d97-26f0ba7c8a07',
+    artist: {
+        name: 'Gorillaz',
+        synopsis: 'Everyone agreed after Gorillaz blew the roof off the Werchter stage in 2018 - the brainchild of comic artist Jamie Hewlett and Blur’s Damon Albarn is one of the best live acts ever. Some of the headlines: ‘So much grandeur’. ‘An infectious carnival’. ‘Dancing towards a better world’. Shortly afterwards, Albarn announced that we shouldn’t expect too much over the next ten years. Then in January this year, they presented the musical concept ‘Song Machine’. And next year, the first virtual band in history is heading back out on tour. From now on, Gorillaz will release music in episodes, whenever new material is ready. Each episode features a different guest:  in 2020 the likes of slowthai, Fatoumata Diaware, Peter Hook (New Order), Georgia and Octavian joined forces with the band.',
+        social: {
+            website: 'https://www.gorillaz.com/',
+            facebook: 'https://www.facebook.com/Gorillaz',
+            twitter: 'https://www.twitter.com/gorillaz',
+            instagram: 'https://www.instagram.com/gorillaz',
+          },
+      
+      picture: {
+          small: 'https://assets.rockwerchter.be/files/cache/medium/files/gorillaz-5f030ee8605de.jpg',
+          large: 'https://assets.rockwerchter.be/files/cache/medium/files/gorillaz-5f030ee8605de.jpg'
+      },
+      media: [
+          {
+            type: 'Youtube',
+            sourceld: 'https://www.youtube.com/embed/O1ALIgizTcc'
+          },
+      ],
+    },
+      place: {
+          name: 'Klub C'
+      },
+      from: 1625263200000,
+      to: 1625263200000,
+      createdAt: 1603490400000,
+      modifietAt: 1604790000000,
+      isHeadliner: false,
+  },
+  {
+    id: '615e375c-eea1-48bc-9865-f97e86df6047',
+    artist: {
+        name: 'girl in red',
+        synopsis: 'In September 2017, Marie Ulven Ringheim realised she was in love with her best friend. She saw her in the distance at a concert and sent a text saying ‘girl in red’. Nothing came of it. But that moment of excitement and longing had a strong influence on her music from then on, says the Norwegian in her twenties. And she also had a readymade name for the songs she wrote in her bedroom on the guitar her grandfather had given her for her birthday when she was a teenager. Her first single ‘I Wanna Be Your Girlfriend’ was considered one of the best songs of 2018. It expressed the aspirations of many of her contemporaries and turned Ringheim into a beacon for the LGBT community. That may seem weighty but her songs are nimble and humorous, first and foremost. Her debut album ‘World In Red’ comes out in October.',
+        social: {
+            website: 'https://www.worldinred.com/',
+            facebook: '',
+            twitter: 'https://www.twitter.com/_girlinred_',
+            instagram: ''
+            
+          },
+      
+      picture: {
+          small: 'https://assets.rockwerchter.be/files/cache/medium/files/girl-in-redd-1-5f030ee7c3796.jpg',
+          large: 'https://assets.rockwerchter.be/files/cache/medium/files/girl-in-redd-1-5f030ee7c3796.jpg'
+      },
+      media: [
+          {
+            type: 'Youtube',
+            sourceld: 'https://www.youtube.com/embed/Pzq4TEU-wHo'
+          },
+      ],
+    },
+      place: {
+          name: 'Klub C'
+      },
+      from: 1625176800000,
+      to: 1625176800000,
+      createdAt: 1603490400000,
+      modifietAt: 1604790000000,
+      isHeadliner: false,
+  },
+];
+
+const mediabar = [
+
+        {
+            name: 'facebook',
+            link: 'https://www.facebook.com',
+            type: 'fa fa-facebook',
+        },
+        {
+            name: 'twitter',
+            link: 'https://twitter.com',
+            type: 'fa fa-twitter',
+        },
+        {
+            name: 'instagram',
+            link: 'https://www.instagram.com',
+            type: 'fa fa-instagram',
+        },
+        {
+            name: 'spotify',
+            link: 'https://www.spotify.com/be-nl',
+            type: 'fa fa-spotify',
+        },
+        {
+            name: 'youtube',
+            link: 'https://www.youtube.com',
+            type: 'fa fa-youtube',
+        },
+    
+];
+
+const info = [
+            {
+                text1: 'About Rock Werchter',
+                link: 'https://www.rockwerchter.be/en/about-rock-werchter',
+            },
+            {
+                text1: 'Contact',
+                link: 'https://www.rockwerchter.be/en/contact',
+            },
+            {
+                text1: 'Press website',
+                link: 'https://www.rockwerchter.be/en/press-website',
+            },
+            {
+                text1: 'Conditions of sale',
+                link: 'https://www.rockwerchter.be/en/conditions-of-sale',
+            },
+            {
+                text1: 'Green',
+                link: 'https://www.rockwerchter.be/en/green',
+            },
+            {
+                text1: 'Disclaimer',
+                link: 'https://www.rockwerchter.be/en/disclaimer',
+            },
+            {
+                text1: 'Privacy policy',
+                link: 'https://nl.livenation.be/privacy?_ga=2.232750808.653004078.1604587736-1272250283.1603792803',
+            },
+            {
+                text1: 'Cookies', 
+                link: 'https://nl.livenation.be/cookies?_ga=2.232750808.653004078.1604587736-1272250283.1603792803',
+            },
+            {
+                text1: 'Thanks to',
+                link: 'https://www.rockwerchter.be/en/thanks-to',
+            },
+];
+
+const knowMore = [
+            {
+                text1: 'Tickets',
+                link: 'https://www.rockwerchter.be/en/practical/tickets',
+            },
+            {
+                text1: 'At the festival site',
+                link: 'https://www.rockwerchter.be/en/practical/at-the-festival-site',
+            },
+            {
+                text1: 'How to get there?',
+                link: 'https://www.rockwerchter.be/en/practical/how-to-get-there',
+            },
+            {
+                text1: 'Camping',
+                link: 'https://www.rockwerchter.be/en/practical/camping',
+            },
+            {
+                text1: 'Frequently asked questions',
+                link: 'https://www.rockwerchter.be/en/practical/frequently-asked-questions',
+            },
+];
+
+const newsLetter = [
+    {
+        title: 'NEWSLETTER',
+    
+        text: 'Subscribe to our newsletter to stay tuned to the latest news.',
+    },
+];
